57ccd4076166815be4b6a0c91627b5197d68ba79
Divyansh-dump <divyanshg917@gmail.com>
2024-09-25 22:42:21 +0530

    Update Program_2.java

--- OOP_Lab/Lab_1/Program_2.java
+++ OOP_Lab/Lab_1/Program_2.java
@@ -1,20 +1,16 @@
 package Lab_1;
 import java.util.*;
-public class Program_2 
-{
-	static int reverse(int n)
-	{
+public class Program_2 {
+	static int reverse(int n) {
 		int reverse =0;
-		while(n>0)
-		{
+		while(n>0) {
 			int digit = n%10;
 			reverse = (reverse * 10)+ digit;
 			n=n/10;
 		}
 		return reverse;
 	}
-	public  static void main(String[] args)
-	{
+	public  static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int value, rev;
 		System.out.println("Enter a number");
